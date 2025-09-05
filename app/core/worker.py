93e85f1a2613820545a24818255c56e89f7749e7
Макс <rumyantsev1m0s1@gmail.com>
2025-09-05 19:42:58 +0300

    удаление worker.py так как запуск воркера происходит через консоль

--- app/core/worker.py
+++
@@ -1,8 +0,0 @@
-from taskiq.cli.worker.run import run_worker
-
-from app.core.lifespan import broker
-
-
-if __name__ == '__main__':
-    # taskiq worker app.core.lifespan:broker --fs-discover --tasks-pattern="app/backend/routs.py"
-    run_worker(broker)
